f95ae7291b83eac93699a6308040d37bddc83d04
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-04-26 13:29:13 +0200

    finshing the app

--- server/server.js
+++ server/server.js
@@ -1,17 +1,46 @@
 const path = require('path');
-const http = require('http');
+
 const express = require('express');
-const socketIO = require('socket.io');
+const app = express();
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT,()=>{
+  console.log(`listening on ${PORT}`)
+})
+
+const io = require('socket.io')(server)
 
 const publicPath = path.join(__dirname, '/../public');
 
-const app = express();
+let socketsConnected = new Set()
 
-const PORT = process.env.PORT || 3000
+
 
 app.use(express.static(publicPath));
 
 
-app.listen(PORT,()=>{
-  console.log(`listening on ${PORT}`)
-})
+io.on('connection', onConnected)
+
+function onConnected(socket){
+  console.log(socket.id)
+  socketsConnected.add(socket.id)
+
+  io.emit('clients-total',socketsConnected.size)
+
+  socket.on('disconnect',()=>{
+    console.log('Socket disconnected',socket.id)
+    socketsConnected.delete(socket.id)
+
+
+    io.emit('clients-total',socketsConnected.size)
+  })
+
+  socket.on('message',(data)=>{
+    console.log(data)
+    socket.broadcast.emit('chat-message',data)
+  })
+
+  socket.on('feedback',(data)=>{
+    socket.broadcast.emit('feedback',data)
+  })
+}
+
